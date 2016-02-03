f4c84a8c68069c3bb542ca4ee28d01449e9d2751
Henning Leutz <leutz@pcsg.de>
2016-02-03 07:31:43 +0100

    login url

--- bin/controls/AuthSettings.js
+++ bin/controls/AuthSettings.js
@@ -31,17 +31,24 @@ define('package/quiqqer/twitter/bin/controls/AuthSettings', [
          * event: on inject
          */
         $onImport: function () {
-            var Elm = this.getElm(),
-                Url = Elm.getElement('.callback-url');
+            var Elm   = this.getElm(),
+                Url   = Elm.getElement('.callback-url'),
+                Login = Elm.getElement('.login-url');
 
             var callbackUrl = window.location.toString().replace(URL_SYS_DIR, '') +
                               URL_OPT_DIR +
                               'quiqqer/twitter/bin/auth/callback.php';
 
+            var loginUrl = window.location.toString().replace(URL_SYS_DIR, '') +
+                           URL_OPT_DIR +
+                           'quiqqer/twitter/bin/auth/login.php';
+
             if (Url) {
                 Url.set('html', callbackUrl);
+                Login.set('html', loginUrl);
             } else {
                 console.info(callbackUrl);
+                console.info(loginUrl);
             }
         }
     });
