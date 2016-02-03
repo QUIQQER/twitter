d420109db76c4e9205fc5b56abbf047677944d99
Henning Leutz <leutz@pcsg.de>
2016-02-03 07:29:09 +0100

    app login

--- bin/auth/callback.php
+++ bin/auth/callback.php
@@ -54,6 +54,43 @@ try {
         $Session->del('oauth_token');
         $Session->del('oauth_token_secret');
 
+        $userData = $Connection->get('account/verify_credentials');
+
+        $result = QUI::getDataBase()->fetch(array(
+            'from' => QUI\Twitter\Handler::getUserDatabaseTableName(),
+            'where' => array(
+                'oauth_uid' => $userData->id
+            )
+        ));
+
+        if (!isset($result[0])) {
+            QUI::getDataBase()->insert(
+                QUI\Twitter\Handler::getUserDatabaseTableName(),
+                array(
+                    'uid' => $User->getId(),
+                    'oauth_uid' => $userData->id,
+                    'oauth_provider' => 'twitter',
+                    'oauth_token' => $access_token['oauth_token'],
+                    'oauth_secret' => $access_token['oauth_token_secret'],
+                    'username' => $userData->screen_name
+                )
+            );
+        } else {
+            QUI::getDataBase()->update(
+                QUI\Twitter\Handler::getUserDatabaseTableName(),
+                array(
+                    'uid' => $User->getId(),
+                    'oauth_provider' => 'twitter',
+                    'oauth_token' => $access_token['oauth_token'],
+                    'oauth_secret' => $access_token['oauth_token_secret'],
+                    'username' => $userData->screen_name
+                ),
+                array(
+                    'oauth_uid' => $userData->id
+                )
+            );
+        }
+
     } else {
         $Session->destroy();
         header('Location: ' . URL_DIR);
